bebd942e21d432f2c58250541b2691c27d5ae7a1
Nandha kumar S <mailstonandhakumar@gmail.com>
2019-06-07 10:54:51 +0530

    Update dhoni.py

--- dhoni.py
+++ dhoni.py
@@ -1,12 +1,17 @@
 str1="dhoni"
 str2=input()
 c=0 
+status=0
 for i in range(0,len(str1)):
     for j in range(0,len(str2)):
-        if(str1[i]==str2[j]):
-            str1[i].replace(str1[i],'*')
-            c+=1
-if(len(str1)==c):
+        if(str2[j] in str1):
+            if(str1[i]==str2[j]):
+                str1[i].replace(str1[i],'*')
+                c+=1
+        else:
+            status=1
+        
+if(len(str1)==c and status==0):
     print('yes')
 else:
     print('no')
